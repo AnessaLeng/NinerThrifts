51611c5ccd63757bb8f34a8fdd47f2b967fe5792
Cindy Nguyen <cindynguyenn77@gmail.com>
2024-03-26 21:56:30 -0400

    create post html template w app.py

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/create_post', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        price = request.form.get('price')
+        hashtags = request.form.get('hashtags')
+        description = request.form.get('description')
+
+        print("Title:", title)
+        print("Price:", price)
+        print("Hashtags:", hashtags)
+        print("Description:", description)
+        return "You have succesfully created a listing!"
+
+    return render_template('create_post.html')
+
 
