8dc5e3e17fe8e7e787ca5176e07c4ffe0ba13f9e
Cindy Nguyen <cindynguyenn77@gmail.com>
2024-03-27 18:20:51 -0400

    fixed the _layout.html page

--- app.py
+++ app.py
@@ -8,24 +8,6 @@ profile_info = {}
 def index():
     return render_template('index.html')
 
-@app.route('/profile')
-def show_profile():
-    user_pic = "static/user_icon.png"
-    username = "username here"
-    bio = "bio here"
-    followers = "###"
-    following = "###"
-    posts= ['static/placeholder.png', 'static/placeholder.png', 'static/placeholder.png', 'static/placeholder.png',
-            'static/placeholder.png','static/placeholder.png','static/placeholder.png','static/placeholder.png']
-    profile_info[username] = []
-    profile_info[username].append(user_pic)
-    profile_info[username].append(bio)
-    profile_info[username].append(followers)
-    profile_info[username].append(following)
-    return render_template("profile.html", profile_info = profile_info, posts = posts)
-
-users = {}
-
 # Anessa's signup/login feature
 @app.route('/')
 def index():
