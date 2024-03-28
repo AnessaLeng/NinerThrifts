caca059897fcc5cbb8e0497d988f3fd828fbc9ce
Varsha Chintalapati <varshachintalapati@MacBook-Pro-26.lan>
2024-03-27 22:05:42 -0400

    adding the route in app.py

--- app.py
+++ app.py
@@ -23,3 +23,11 @@ def show_profile():
     profile_info[username].append(followers)
     profile_info[username].append(following)
     return render_template("profile.html", profile_info = profile_info, posts = posts)
+
+@app.route('/individual_post')
+def show_post():
+    post_image = posts[post_id -1]
+    post_title = "Placeholder Title"
+    post_price = "$Placeholder Price"
+    post_description = "Placeholder Description"
+    return render_template('individual_post.html', post_image=post_image, post_title=post_title, post_price=post_price, post_description=post_description)
