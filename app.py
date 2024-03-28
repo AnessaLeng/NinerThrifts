e46c9c6cda5c4deb1d7c5a6e0ba18e446dede28f
Varsha Chintalapati <varshachintalapati@MacBook-Pro-26.lan>
2024-03-27 22:51:35 -0400

    finished basic template of page

--- app.py
+++ app.py
@@ -1,3 +1,28 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+profile_info = {}
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile')
+def show_profile():
+    user_pic = "static/user_icon.png"
+    username = "username here"
+    bio = "bio here"
+    followers = "###"
+    following = "###"
+    posts= ['static/placeholder.png', 'static/placeholder.png', 'static/placeholder.png', 'static/placeholder.png',
+            'static/placeholder.png','static/placeholder.png','static/placeholder.png','static/placeholder.png']
+    profile_info[username] = []
+    profile_info[username].append(user_pic)
+    profile_info[username].append(bio)
+    profile_info[username].append(followers)
+    profile_info[username].append(following)
+    return render_template("profile.html", profile_info = profile_info, posts = posts)
 from flask import Flask, redirect, render_template, request
 
 
@@ -72,7 +97,7 @@ def create_post():
 
 @app.route('/individual_post')
 def show_post():
-    post_image = posts[post_id -1]
+    post_image = 'static/placeholder.png'
     post_title = "Placeholder Title"
     post_price = "$Placeholder Price"
     post_description = "Placeholder Description"
