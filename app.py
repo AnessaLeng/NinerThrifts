bc33a88345f2b99efd2c6fe6c5a607d7abaf1341
Cindy Nguyen <cindynguyenn77@gmail.com>
2024-04-09 17:37:33 -0400

    basic outline of favorites pg + css

--- app.py
+++ app.py
@@ -113,3 +113,15 @@ def search():
     search_result = request.form['query']
     #to do: get results from database
     return render_template("search.html", search_result = search_result)
+
+
+@app.route('/favorites', methods=["GET"])
+def favorites():
+    # will change this after pulling posts from database
+    post = "static/blankpost.jpg"
+    post_id = "post id"
+    posts = ["static/blankpost.jpg", "static/blankpost.jpg", "static/blankpost.jpg", "static/blankpost.jpg", 
+            "static/blankpost.jpg", "static/blankpost.jpg", "static/blankpost.jpg", "static/blankpost.jpg"]
+    postGrid[post_id] = []
+    postGrid[post_id].append(post)
+    return render_template("favorites.html", postGrid = postGrid, posts = posts)
