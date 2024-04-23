10eb6b369ff149b307444634e5bc9ae007fc5860
Jaiden Daya <jaidendaya@gmail.com>
2024-04-23 14:25:18 -0400

    Testing database

--- repositories/profile_repo.py
+++ repositories/profile_repo.py
@@ -1,24 +1,21 @@
-# from repositories.db import get_pool
-# from psycopg.rows import dict_row
+from repositories.db import get_pool
+from psycopg.rows import dict_row
 
-#***********************************************************
-# THIS WILL BE IMPLEMENTED (uncommented) WHEN WE HAVE A DATABASE NEXT SPRINT
-#************************************************************
 
-# def get_profile_info():
-#     pool = get_pool()
-#     with pool.connection() as conn:
-#         with conn.cursor(row_factory=dict_row) as cursor:
-#             cursor.execute('''      
-#                            SELECT
-#                                 user_id,
-#                                 username,
-#                                 biography,
-#                                 profile_picture,
-#                                 followers,
-#                                 following
-#                            FROM
-#                                 profile
-#                             ''')
-#             return cursor.fetchall()
+def get_profile_info():
+    pool = get_pool()
+    with pool.connection() as conn:
+        with conn.cursor(row_factory=dict_row) as cursor:
+            cursor.execute('''      
+                           SELECT
+                                user_id,
+                                username,
+                                biography,
+                                profile_picture,
+                                followers,
+                                following
+                           FROM
+                                profiles
+                            ''')
+            return cursor.fetchall()
 
