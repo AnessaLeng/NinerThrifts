9944ded79980ae26bf4e61a67f366bd42a5db136
Varsha Chintalapati <varshachintalapati@MacBook-Pro-26.local>
2024-05-02 19:09:44 -0400

    fixes

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ def create_listing():
     return render_template('create_post.html')
 
 # Edit post route
-@app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
+@app.route('/edit_post/<post_id>', methods=['GET', 'POST'])
 def edit_post(post_id):
     post = post_repo.get_post_by_id(post_id)
     if request.method == 'POST':
@@ -233,7 +233,7 @@ def edit_post(post_id):
 
 
 # Delete post route
-@app.route('/delete_post/<int:post_id>', methods=['POST'])
+@app.route('/delete_post/<post_id>', methods=['POST'])
 def delete_post(post_id):
     # Check if the request method is POST
     if request.method == 'POST':
